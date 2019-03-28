e56f50f050ff0e09fd9ec9c6afacb613e7ea6b8c
Artemiy Tyuplyaev <38858007+liuminol@users.noreply.github.com>
2019-03-28 18:38:43 +0300

    screen resolution check added

--- js/title-hover.js
+++ js/title-hover.js
@@ -2,24 +2,26 @@
 
 ;
 (function () {
-  var title = document.querySelector('.wrapper__title');
-  var color;
+  var REQUIRED_SCREEN_SIZE = 768;
 
-  var getRandNum = function (max) {
-    return Math.round(Math.random() * max);
-  };
-  
-  var changeColor = function() {
-    title.classList.remove('animation--fadein');
-    
-    color = 'rgb(' + getRandNum(255) + ', ' + getRandNum(230) + ', ' + getRandNum(255) + ')';
-    title.style.color = color;
-  };
-  
-  
-  title.classList.add('animation--fadein');
+  if (window.screen.width > REQUIRED_SCREEN_SIZE) {
+    var title = document.querySelector('.wrapper__title');
+    var color;
 
-  title.addEventListener('mouseover', function () {
-    window.debounce(changeColor);
-  });
+    var getRandNum = function (max) {
+      return Math.round(Math.random() * max);
+    };
+
+    var changeColor = function () {
+      color = 'rgb(' + getRandNum(255) + ', ' + getRandNum(230) + ', ' + getRandNum(255) + ')';
+      title.style.color = color;
+    };
+
+    title.classList.add('animation--fadein');
+
+    title.addEventListener('mouseover', function () {
+      title.classList.remove('animation--fadein');
+      window.debounce(changeColor);
+    });
+  };
 }());
